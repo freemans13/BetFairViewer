b18ad15238203deada93284d94c1089a77a95573
freemans13 <stu@team-freeman.com>
2023-07-21 18:36:22 +0100

    Show num of matched and total selections plus green Up profit

--- src/MarketList.jsx
+++ src/MarketList.jsx
@@ -1,19 +1,8 @@
+import { useOverview } from './api';
 import React from 'react';
 
 export default function MarketList({ setMarket }) {
-  const [markets, setMarkets] = React.useState([]);
-
-  React.useEffect(() => {
-    async function fetchMarketCatalogue() {
-      const result = await fetch('betfair/MarketCatalogue');
-      const json = await result.json();
-      setMarkets(json);
-    }
-    fetchMarketCatalogue();
-    setInterval(() => {
-      fetchMarketCatalogue();
-    }, 60_000);
-  }, []);
+  const { data: markets, isLoading, error } = useOverview();
 
   const options = {
     timeZone: 'Europe/London',
@@ -24,23 +13,75 @@ export default function MarketList({ setMarket }) {
     const londonTime = new Date(date).toLocaleTimeString('en-US', options);
     return londonTime;
   }
+
+  function getMatchedCount(market) {
+    const matchedCount = market.payout.payouts.reduce((acc, payout) => {
+      if (!payout.matched) {
+        return acc;
+      }
+      return acc + 1;
+    }, 0);
+    return matchedCount;
+  }
+
+  function getUnmatchedCount(market) {
+    const unmatchedCount = market.payout.payouts.reduce((acc, payout) => {
+      if (payout.void) {
+        return acc;
+      }
+      if (payout.matched) {
+        return acc;
+      }
+      return acc + 1;
+    }, 0);
+    return unmatchedCount;
+  }
+
+  function getGreenUpValue(market) {
+    const greenUpValue = market.greenUp.payouts.reduce((acc, payout) => {
+      if (payout.void) {
+        return acc;
+      }
+      if (!payout.profit) {
+        return acc;
+      }
+      return Math.min(acc, payout.profit);
+    }, Number.MAX_VALUE);
+    if (greenUpValue === Number.MAX_VALUE) {
+      return 0;
+    }
+    return greenUpValue;
+  }
+
   return (
     <>
-      {markets.map((market) => {
-        const startTime = new Date(market.marketStartTime);
-        const old = startTime < new Date();
-        const style = old ? { color: 'gray' } : {};
-        return (
-          <button
-            key={market.marketId}
-            type="button"
-            onClick={() => setMarket(market)}
-            style={style}
-          >
-            {market.event.name} {getRaceLocalTime(market.marketStartTime)} {market.marketId}
-          </button>
-        );
-      })}
+      {markets &&
+        markets.map((market) => {
+          const startTime = new Date(market.marketStartTime);
+          const old = startTime < new Date();
+          const style = old ? { color: 'gray' } : {};
+          const greenUpValue = getGreenUpValue(market);
+          const matched = getMatchedCount(market);
+          const total = getUnmatchedCount(market) + matched;
+          return (
+            <button
+              key={market.marketId}
+              type="button"
+              onClick={() => setMarket(market)}
+              style={style}
+            >
+              {market.event.name} {getRaceLocalTime(market.marketStartTime)}{' '}
+              <p>
+                {market.marketId} {matched}/{total}
+              </p>
+              <p style={redOrGreen(greenUpValue)}>£{greenUpValue.toFixed(2)}</p>
+            </button>
+          );
+        })}
     </>
   );
+}
+
+function redOrGreen(value) {
+  return { color: `${value >= 0 ? 'lightgreen' : 'red'}` };
 }
--- src/api.js
+++ src/api.js
@@ -1,10 +1,24 @@
 /* eslint-disable no-unused-vars */
 import useSWR from 'swr';
 
-function useMarketProjection(marketId) {
-  // @ts-ignore
-  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+// @ts-ignore
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
+function useOverview() {
+  const response = useSWR('/betfair/Overview', fetcher, {
+    refreshInterval: 5_000, // Poll every few seconds
+  });
+  return response;
+}
+
+function useMarketCatalogue() {
+  const response = useSWR('/betfair/MarketCatalogue', fetcher, {
+    refreshInterval: 60_000, // Poll every 60 seconds
+  });
+  return response;
+}
+
+function useMarketProjection(marketId) {
   const { data, isLoading, error } = useSWR(`/betfair/market/${marketId}`, fetcher, {
     refreshInterval: 1000, // Poll every second (1000ms)
   });
@@ -12,13 +26,10 @@ function useMarketProjection(marketId) {
 }
 
 function useBook(marketId) {
-  // @ts-ignore
-  const fetcher = (...args) => fetch(...args).then((res) => res.json());
-
   const { data, isLoading, error } = useSWR(`/betfair/market/${marketId}/book`, fetcher, {
     refreshInterval: 1000, // Poll every second (1000ms)
   });
   return { data, isLoading, error };
 }
 
-export { useMarketProjection, useBook };
+export { useOverview, useMarketCatalogue, useMarketProjection, useBook };
