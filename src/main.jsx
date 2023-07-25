0bd316c2187e2022bd7f2cd1026f8665486ab949
freemans13 <stu@team-freeman.com>
2023-07-25 15:22:31 +0100

    Typo

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import GlobalStyles from './GlobalStyles';
 import './index.css';
 import RunnerList from './RunnerList.jsx';
-import Overview from './Overview.jsx';
+import Overview from './OvervIew.jsx';
 
 async function OverviewLoader() {
   const markets = await fetch('/betfair/Overview').then((r) => r.json());
